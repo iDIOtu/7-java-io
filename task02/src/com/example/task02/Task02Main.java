2eb1123f738367c5e2c7bc785971a3190d6e463f
iDIOtu <95068240+iDIOtu@users.noreply.github.com>
2024-11-07 19:33:30 +0500

    task02

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -9,5 +9,23 @@ public class Task02Main {
         // - направить стандартный вывод программы в файл output.test
         // - запустить программу
         // - и сравнить получившийся файл output.test с expected.test
+        ConvertToUnix();
+    }
+
+    public static void ConvertToUnix() throws IOException {
+
+        int currentStroke = System.in.read();
+        int nextStroke;
+
+        while(currentStroke != -1){
+            nextStroke = System.in.read();
+
+            if (currentStroke != 13 || nextStroke != 10)
+                System.out.write(currentStroke);
+
+            currentStroke = nextStroke;
+        }
+
+        System.out.flush();
     }
 }
